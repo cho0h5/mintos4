626ac98dbd83acafc789603cd96833500c684e0f
Youngho Cho <dominoyh5@gmail.com>
2025-06-27 18:15:29 +0900

    feat: init memory for ia-32e

--- 01.Kernel32/Source/Main.c
+++ 01.Kernel32/Source/Main.c
@@ -1,9 +1,17 @@
 #include "Types.h"
 
 void kPrintString(const int iX, const int iY, const char *pcString);
+BOOL kInitializeKernel64Area();
 
 void Main() {
     kPrintString(0, 3, "C Lang Kernel Started");
+
+    if(kInitializeKernel64Area()) {
+        kPrintString(0, 4, "IA-32e Kernel Area Initialization Complete");
+    } else {
+        kPrintString(0, 4, "IA-32e Kernel Area Initialization Fail");
+        while (1) ;
+    }
 
     while (1) ;
 }
@@ -15,4 +23,20 @@ void kPrintString(const int iX, const int iY, const char *pcString) {
     for (int i = 0; pcString[i] != '\0'; i++) {
         pstScreen[i].bCharactor = pcString[i];
     }
+}
+
+BOOL kInitializeKernel64Area() {
+    DWORD *pdwCurrentAddress = (DWORD *)0x100000;
+
+    while (pdwCurrentAddress < 0x600000) {
+        *pdwCurrentAddress = 0x00;
+
+        if (*pdwCurrentAddress != 0) {
+            return FALSE;
+        }
+
+        pdwCurrentAddress++;
+    }
+
+    return TRUE;
 }
