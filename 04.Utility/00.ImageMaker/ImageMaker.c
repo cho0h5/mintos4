bc0bead81c7879acc9dc0a8b406adfd41c1212e6
Youngho Cho <dominoyh5@gmail.com>
2025-06-27 16:53:29 +0900

    feat: add ImageMaker

---
+++ 04.Utility/00.ImageMaker/ImageMaker.c
@@ -0,0 +1,123 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+
+#define BYTEOFSECTOR 512
+
+int AdjustInSectorSize(int iFd, int iSourceSize);
+void WriteKernelInformation(int iTargetFd, int iKernelSectorCount);
+int CopyFile(int iSourceFd, int iTargetFd);
+
+int main(int argc, char *argv[]) {
+    int iSourceFd;
+    int iTargetFd;
+    int iBootLoaderSize;
+    int iKernel32SectorCount;
+    int iSourceSize;
+
+    if (argc < 3) {
+        fprintf(stderr, "[ERROR] ImageMaker BootLoader.bin Kernel32.bin\n");
+        exit(1);
+    }
+
+    // Open Disk.img
+    if ((iTargetFd = open("Disk.img", O_RDWR | O_CREAT | O_TRUNC, S_IREAD | S_IWRITE)) == -1) {
+        fprintf(stderr, "[ERROR] Disk.img open fail.\n");
+        exit(1);
+    }
+
+    // BootLoader
+    printf("[INFO] Copy BootLoader to Disk.img\n");
+    if ((iSourceFd = open(argv[1], O_RDONLY)) == -1) {
+        fprintf(stderr, "[ERROR] %s open fail\n", argv[1]);
+        exit(1);
+    }
+
+    iSourceSize = CopyFile(iSourceFd, iTargetFd);
+    close(iSourceFd);
+
+    iBootLoaderSize = AdjustInSectorSize(iTargetFd, iSourceSize);
+    printf("[INFO] %s size = [%d] and sector count = [%d]\n", argv[1], iSourceSize, iBootLoaderSize);
+
+    // Kernel32
+    printf("[INFO] Copy Kernel32 to Disk.img\n");
+    if ((iSourceFd = open(argv[2], O_RDONLY)) == -1) {
+        fprintf(stderr, "[ERROR] %s open fail\n", argv[2]);
+        exit(1);
+    }
+
+    iSourceSize = CopyFile(iSourceFd, iTargetFd);
+    close(iSourceFd);
+
+    iKernel32SectorCount = AdjustInSectorSize(iTargetFd, iSourceSize);
+    printf("[INFO] %s size = [%d] and sector count = [%d]\n", argv[2], iSourceSize, iKernel32SectorCount);
+
+    // Update SectorSize
+    printf("[INFO] Start to write kernel information\n");
+    WriteKernelInformation(iTargetFd, iKernel32SectorCount);
+    printf("[INFO] Image file create complete\n");
+
+    close(iTargetFd);
+    return 0;
+}
+
+int AdjustInSectorSize(const int iFd, int iSourceSize) {
+    int iAdjustSizeToSector = iSourceSize % BYTEOFSECTOR;
+    char cCh = 0x00;
+
+    if (iAdjustSizeToSector != 0) {
+        iAdjustSizeToSector = 512 - iAdjustSizeToSector;
+        printf("[INFO] File size [%lu] and fill [%u] byte\n", iSourceSize, iAdjustSizeToSector);
+
+        for (int i = 0; i < iAdjustSizeToSector; i++) {
+            write(iFd, &cCh, 1);
+        }
+    } else {
+        printf("[INFO] File size is aligned 512 byte\n");
+    }
+
+    int iSectorCount = (iSourceSize + iAdjustSizeToSector) / BYTEOFSECTOR;
+    return iSectorCount;
+}
+
+void WriteKernelInformation(int iTargetFd, int iKernelSectorCount) {
+    unsigned short usData;
+    long lPosition;
+
+    lPosition = lseek(iTargetFd, (off_t)5, SEEK_SET);
+    if (lPosition == -1) {
+        fprintf(stderr, "lseek fail. Return value = %d, errno = %d, %d\n", lPosition, errno, SEEK_SET);
+        exit(1);
+    }
+
+    usData = (unsigned short)iKernelSectorCount;
+    write(iTargetFd, &usData, 2);
+
+    printf("[INFO] Total sector count except boot loader [%d]\n", iKernelSectorCount);
+}
+
+int CopyFile(int iSourceFd, int iTargetFd) {
+    int iSourceFileSize = 0;
+    
+    while (1) {
+        char vcBuffer[BYTEOFSECTOR];
+        int iRead = read(iSourceFd, vcBuffer, sizeof(vcBuffer));
+        int iWrite = write(iTargetFd, vcBuffer, iRead);
+
+        if (iRead != iWrite) {
+            fprintf(stderr, "[ERROR] iRead != iWrite\n");
+            exit(1);
+        }
+        iSourceFileSize += iRead;
+
+        if (iRead != sizeof(vcBuffer)) {
+            break;
+        }
+    }
+
+    return iSourceFileSize;
+}
--- Makefile
+++ Makefile
@@ -9,13 +9,19 @@ BootLoader:
 Kernel32:
 	make -C 01.Kernel32
 
-Disk.img: BootLoader Kernel32
-	cat 00.BootLoader/BootLoader.bin 01.Kernel32/Kernel32.bin > Disk.img
+ImageMaker:
+	make -C 04.Utility/00.ImageMaker
+	cp 04.Utility/00.ImageMaker/ImageMaker .
+
+Disk.img: BootLoader Kernel32 ImageMaker
+	./ImageMaker 00.BootLoader/BootLoader.bin 01.Kernel32/Kernel32.bin
 
 clean:
 	make -C 00.BootLoader clean
 	make -C 01.Kernel32 clean
+	make -C 04.Utility/00.ImageMaker clean
 	rm -f Disk.img
+	rm -f ImageMaker
 
 re:
 	make clean
