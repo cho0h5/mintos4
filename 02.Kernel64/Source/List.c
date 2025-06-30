6115480be1fe7f6c436d4d1929f73814f7c48b72
Youngho Cho <dominoyh5@gmail.com>
2025-06-30 15:36:55 +0900

    feat: impl List

---
+++ 02.Kernel64/Source/List.c
@@ -0,0 +1,79 @@
+#include "List.h"
+
+void kAddListToTail(LIST *pstList, void *pvItem) {
+    LISTLINK *pstLink = (LISTLINK *)pvItem;
+    pstLink->pvNext = NULL;
+
+    if (pstList->pvHeader == NULL) {
+        pstList->pvHeader = pvItem;
+        pstList->pvTail = pvItem;
+        pstList->iItemCount = 1;
+        return;
+    }
+
+    pstLink = (LISTLINK *)pstList->pvTail;
+    pstLink->pvNext = pvItem;
+
+    pstList->pvTail = pvItem;
+    pstList->iItemCount++;
+}
+
+void kAddListToHeader(LIST *pstList, void *pvItem) {
+    LISTLINK *pstLink = (LISTLINK *)pvItem;
+    pstLink->pvNext = pstList->pvHeader;
+
+    if (pstList->pvHeader == NULL) {
+        pstList->pvHeader = pvItem;
+        pstList->pvTail = pvItem;
+        pstList->iItemCount = 1;
+        return;
+    }
+
+    pstList->pvHeader = pvItem;
+    pstList->iItemCount++;
+}
+
+void *kRemoveList(LIST *pstList, QWORD qwID) {
+    LISTLINK *pstPreviousLink = (LISTLINK *)pstList->pvHeader;
+    LISTLINK *pstLink = pstPreviousLink;
+    for (;pstLink != NULL; pstLink = pstLink->pvNext) {
+        if (pstLink->qwID != qwID) {
+            pstPreviousLink = pstLink;
+            continue;
+        }
+
+        if (pstLink == pstList->pvHeader && pstLink == pstList->pvTail) {
+            pstList->pvHeader = NULL;
+            pstList->pvTail = NULL;
+        } else if (pstLink == pstList->pvHeader) {
+            pstList->pvHeader = pstLink->pvNext;
+        } else if (pstLink == pstList->pvTail) {
+            pstList->pvTail = pstPreviousLink;
+        } else {
+            pstPreviousLink->pvNext = pstLink->pvNext;
+        }
+
+        pstList->iItemCount--;
+        return pstLink;
+    }
+
+    return NULL;
+}
+
+void *kRemoveListFromHeader(LIST *pstList) {
+    if (pstList->iItemCount == 0) {
+        return NULL;
+    }
+
+    LISTLINK *pstLink = (LISTLINK *)pstList->pvHeader;
+    return kRemoveList(pstList, pstLink->qwID);
+}
+
+void *kRemoveListFromTail(LIST *pstList) {
+    if (pstList->iItemCount == 0) {
+        return NULL;
+    }
+
+    LISTLINK *pstLink = (LISTLINK *)pstList->pvTail;
+    return kRemoveList(pstList, pstLink->qwID);
+}
---
+++ 02.Kernel64/Source/List.h
@@ -0,0 +1,27 @@
+#ifndef __LIST_H__
+#define __LIST_H__
+
+#include "Types.h"
+
+#pragma pack(push, 1)
+
+typedef struct kListLinkStruct {
+    void *pvNext;
+    QWORD qwID;
+} LISTLINK;
+
+typedef struct kListManagerStruct {
+    int iItemCount;
+    void *pvHeader;
+    void *pvTail;
+} LIST;
+
+#pragma pack(pop)
+
+void kAddListToTail(LIST *pstList, void *pvItem);
+void kAddListToHeader(LIST *pstList, void *pvItem);
+void *kRemoveList(LIST *pstList, QWORD qwID);
+void *kRemoveListFromHeader(LIST *pstList);
+void *kRemoveListFromTail(LIST *pstList);
+
+#endif
