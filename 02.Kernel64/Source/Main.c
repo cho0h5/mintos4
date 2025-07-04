c7f3e364df79ac8f1ae7116289c6bf4cd504e129
Youngho Cho <dominoyh5@gmail.com>
2025-07-04 16:47:05 +0900

    fix: remove double operation in main

--- 02.Kernel64/Source/Main.c
+++ 02.Kernel64/Source/Main.c
@@ -66,8 +66,6 @@ void Main() {
     kSetCursor(1, iCursorY++);
     kPrintf("Pass\n");
 
-    kPrintf("hi: %d\n", (double)7 / 2);
-
     kCreateTask(TASK_FLAGS_LOWEST | TASK_FLAGS_THREAD | TASK_FLAGS_SYSTEM | TASK_FLAGS_IDLE,
             0, 0, (QWORD)kIdleTask);
     kStartConsoleShell();
